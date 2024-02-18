463eaf95575924a0aee48428329dcf3a7533b5fc
AnhLeDuc7421 <renyk97@gmail.com>
2024-02-18 15:51:39 +0700

    update

--- routes/site.js
+++ routes/site.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const siteController = require('../controllers/SiteController')
 const { authenMiddleware } = require('../middlewares/AuthenHandler')
 
-router.get('/checkToken',authenMiddleware)
+router.get('/checkToken',siteController.checkAccessToken)
 
 module.exports = router
